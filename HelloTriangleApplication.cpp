93f87fe0a2805df97c5930bceddbd661392f6a53
Dirk Zimmermann <me@dirkz.com>
2025-02-02 19:46:22 +0100

    Hello, window

--- HelloTriangleApplication.cpp
+++ HelloTriangleApplication.cpp
@@ -1,25 +1,47 @@
 #include "stdafx.h"
 
+static const uint32_t Width = 800;
+static const uint32_t Height = 600;
+
 struct HelloTriangleApplication
 {
     void run()
     {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
   private:
+    GLFWwindow *m_window;
+
+    void initWindow()
+    {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        m_window = glfwCreateWindow(Width, Height, "Vulkan", nullptr, nullptr);
+    }
+
     void initVulkan()
     {
     }
 
     void mainLoop()
     {
+        while (!glfwWindowShouldClose(m_window))
+        {
+            glfwPollEvents();
+        }
     }
 
     void cleanup()
     {
+        glfwDestroyWindow(m_window);
+        glfwTerminate();
     }
 };
 
