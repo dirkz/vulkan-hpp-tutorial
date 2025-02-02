f886ef9f9887363a55ec22f80506694d6eabdcd8
Dirk Zimmermann <me@dirkz.com>
2025-02-02 10:00:21 +0100

    Add project files.

---
+++ vk-hpp-tutorial.cpp
@@ -0,0 +1,12 @@
+﻿// vk-hpp-tutorial.cpp : Defines the entry point for the application.
+//
+
+#include "vk-hpp-tutorial.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
