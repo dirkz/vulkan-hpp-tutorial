e86688f6eceee42ddcc9ffbef9c3098ff3e4c02c
Dirk Zimmermann <me@dirkz.com>
2025-02-02 11:30:51 +0100

    Add GLM

--- common/stdafx.h
+++ common/stdafx.h
@@ -1,5 +1,11 @@
 #include <vulkan/vulkan.hpp>
 
+// Include all GLM core / GLSL features
+#include <glm/glm.hpp> // vec2, vec3, mat4, radians
+
+// Include all GLM extensions
+#include <glm/ext.hpp> // perspective, translate, rotate
+
 #include <iostream>
 #include <string>
 #include <vector>
--- vk-hpp-tutorial.cpp
+++ vk-hpp-tutorial.cpp
@@ -6,6 +6,16 @@
 using namespace std;
 using namespace vk;
 
+static glm::mat4 transform(glm::vec2 const &Orientation, glm::vec3 const &Translate, glm::vec3 const &Up)
+{
+    glm::mat4 Proj = glm::perspective(glm::radians(45.f), 1.33f, 0.1f, 10.f);
+    glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.f), Translate);
+    glm::mat4 ViewRotateX = glm::rotate(ViewTranslate, Orientation.y, Up);
+    glm::mat4 View = glm::rotate(ViewRotateX, Orientation.x, Up);
+    glm::mat4 Model = glm::mat4(1.0f);
+    return Proj * View * Model;
+}
+
 int main()
 {
     vector<ExtensionProperties> properties = enumerateInstanceExtensionProperties();
