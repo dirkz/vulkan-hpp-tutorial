c7e453f6b50897614b2b9271d294aff2ab6e9978
Dirk Zimmermann <me@dirkz.com>
2025-02-02 11:43:15 +0100

    Add GLfW, and test it, plus GLM according to the tutorial

--- common/stdafx.h
+++ common/stdafx.h
@@ -1,10 +1,16 @@
 #include <vulkan/vulkan.hpp>
 
+// Make GLM suitable for vulkan
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
 // Include all GLM core / GLSL features
 #include <glm/glm.hpp> // vec2, vec3, mat4, radians
 
 // Include all GLM extensions
 #include <glm/ext.hpp> // perspective, translate, rotate
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <string>
--- vk-hpp-tutorial.cpp
+++ vk-hpp-tutorial.cpp
@@ -6,16 +6,6 @@
 using namespace std;
 using namespace vk;
 
-static glm::mat4 transform(glm::vec2 const &Orientation, glm::vec3 const &Translate, glm::vec3 const &Up)
-{
-    glm::mat4 Proj = glm::perspective(glm::radians(45.f), 1.33f, 0.1f, 10.f);
-    glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.f), Translate);
-    glm::mat4 ViewRotateX = glm::rotate(ViewTranslate, Orientation.y, Up);
-    glm::mat4 View = glm::rotate(ViewRotateX, Orientation.x, Up);
-    glm::mat4 Model = glm::mat4(1.0f);
-    return Proj * View * Model;
-}
-
 int main()
 {
     vector<ExtensionProperties> properties = enumerateInstanceExtensionProperties();
@@ -23,6 +13,24 @@ int main()
     {
         cout << "*** extension" << property.extensionName << "\n";
     }
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
 
     return 0;
 }
