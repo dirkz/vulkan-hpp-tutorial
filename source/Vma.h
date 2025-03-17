05789cf57592d9a927239ef5c505f1c0730a5a84
Dirk Zimmermann <me@dirkz.com>
2025-03-17 11:35:41 +0100

    Put VMA header files in yet another dedicated file

--- source/Vma.h
+++ source/Vma.h
@@ -2,8 +2,7 @@
 
 #include "stdafx.h"
 
-#define VMA_VULKAN_VERSION 1002000 // Vulkan 1.2
-#include <vk_mem_alloc.h>
+#include "VmaIncludes.h"
 
 namespace zvk
 {
---
+++ source/VmaIncludes.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define VMA_VULKAN_VERSION 1002000 // Vulkan 1.2
+#include <vk_mem_alloc.h>
