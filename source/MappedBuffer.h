e73341c18913464bf5fc25eee695c303934c051b
Dirk Zimmermann <me@dirkz.com>
2025-03-17 11:39:16 +0100

    Let the mapped buffer simply derive from the more general vma buffer

--- source/MappedBuffer.h
+++ source/MappedBuffer.h
@@ -2,32 +2,16 @@
 
 #include "stdafx.h"
 
+#include "VmaBuffer.h"
 #include "VmaIncludes.h"
 
 namespace zvk
 {
 
-struct MappedBuffer
+struct MappedBuffer : public VmaBuffer
 {
     MappedBuffer(VmaAllocator allocator, vk::DeviceSize size, vk::BufferUsageFlags usageFlags,
                  vk::SharingMode sharingMode);
-    ~MappedBuffer();
-
-    inline void *Mapped() const
-    {
-        return m_allocationInfo.pMappedData;
-    }
-
-    inline VkBuffer Buffer() const
-    {
-        return m_buffer;
-    }
-
-  private:
-    VmaAllocator m_allocator;
-    VkBuffer m_buffer;
-    VmaAllocation m_allocation;
-    VmaAllocationInfo m_allocationInfo;
 };
 
 } // namespace zvk
