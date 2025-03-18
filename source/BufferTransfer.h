9f5e103e9353076d986ec432067bf01ffa07dc3d
Dirk Zimmermann <me@dirkz.com>
2025-03-18 15:52:43 +0100

    Try a direct uploader

--- source/BufferTransfer.h
+++ source/BufferTransfer.h
@@ -2,7 +2,9 @@
 
 #include "stdafx.h"
 
+#include "MappedBuffer.h"
 #include "QueueFamilyIndices.h"
+#include "Vma.h"
 #include "VmaBuffer.h"
 
 namespace zvk
@@ -14,12 +16,34 @@ struct BufferTransfer
     void Copy(const VmaBuffer &srcBuffer, const VmaBuffer &dstBuffer);
     void FinishAndWait();
 
+    template <class T>
+    VmaBuffer *Upload(const Vma &vma, const std::span<T> contents, vk::BufferUsageFlags usageFlags,
+                      const std::span<uint32_t> queues = {})
+    {
+        auto contentsSize = sizeof(T) * contents.size();
+
+        vk::BufferUsageFlags stagingBufferUsageFlags{vk::BufferUsageFlagBits::eTransferSrc};
+        MappedBuffer *stagingBuffer =
+            vma.NewMappedBuffer(contentsSize, stagingBufferUsageFlags, vk::SharingMode::eExclusive);
+        memcpy(stagingBuffer.Mapped(), contents.data(), contentsSize);
+
+        VmaBuffer *deviceLocalBuffer = vma.NewDeviceLocalBuffer(
+            contentsSize, usageFlags, vk::SharingMode::eConcurrent, queues);
+
+        m_stagingBuffers.push_back(std::unique_ptr{stagingBuffer});
+
+        Copy(*stagingBuffer, *deviceLocalBuffer);
+
+        return deviceLocalBuffer;
+    }
+
   private:
     vk::Device m_device;
     vk::UniqueFence m_fence;
     vk::Queue m_queue;
     vk::UniqueCommandPool m_commandPool;
     vk::UniqueCommandBuffer m_commandBuffer;
+    std::vector<std::unique_ptr<MappedBuffer>> m_stagingBuffers;
 };
 
 } // namespace zvk
