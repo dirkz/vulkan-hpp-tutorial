7700b641eb1a0288875ca1266290aaf0ddb2b268
Dirk Zimmermann <me@dirkz.com>
2025-03-22 16:59:11 +0100

    Uniform buffer that might work

---
+++ source/UniformBuffer.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "stdafx.h"
+
+#include "VmaBuffer.h"
+
+namespace zvk
+{
+
+template <class T> struct UniformBuffer : VmaBuffer
+{
+    UniformBuffer(const VmaAllocator allocator, vk::BufferUsageFlags usageFlags,
+                  vk::SharingMode sharingMode)
+        : VmaBuffer{allocator, sizeof(T), usageFlags,
+                    VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT |
+                        VMA_ALLOCATION_CREATE_MAPPED_BIT,
+                    sharingMode} {};
+
+    inline T *Mapped() const
+    {
+        return static_cast<T *>(m_allocationInfo.pMappedData);
+    }
+};
+
+} // namespace zvk
