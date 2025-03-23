4b81ee54322cb76b25cdf929db5f1bd0f9d090d6
Dirk Zimmermann <me@dirkz.com>
2025-03-23 16:08:02 +0100

    Use #define'd extension names

--- source/Extensions.cpp
+++ source/Extensions.cpp
@@ -38,9 +38,10 @@ std::vector<const char *> RequiredInstanceExtensions()
 
 std::vector<const char *> RequiredDeviceExtensions()
 {
-    static const vector<const char *> requiredDeviceExtensions{VK_KHR_SWAPCHAIN_EXTENSION_NAME,
-                                                               "VK_KHR_dedicated_allocation",
-                                                               "VK_KHR_get_memory_requirements2"};
+    // Note: Extension names can be found in vulkan_core.h
+    static const vector<const char *> requiredDeviceExtensions{
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME, VK_KHR_DEDICATED_ALLOCATION_EXTENSION_NAME,
+        VK_KHR_GET_MEMORY_REQUIREMENTS_2_EXTENSION_NAME};
 
     return requiredDeviceExtensions;
 }
