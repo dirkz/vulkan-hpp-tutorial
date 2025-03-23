4ac2ea7e6e84aedae46b9204ad94da2345814803
Dirk Zimmermann <me@dirkz.com>
2025-03-23 17:15:47 +0100

    Implement the other path

--- source/UniformBuffer.h
+++ source/UniformBuffer.h
@@ -68,6 +68,67 @@ template <class T> struct UniformBuffer
                                  VK_PIPELINE_STAGE_VERTEX_SHADER_BIT, 0, 0, nullptr, 1,
                                  &bufMemBarrier, 0, nullptr);
         }
+        else
+        {
+            // Allocation ended up in a non-mappable memory - a transfer using a staging buffer is
+            // required.
+            VkBufferCreateInfo stagingBufCreateInfo = {VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO};
+            stagingBufCreateInfo.size = 65536;
+            stagingBufCreateInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+
+            VmaAllocationCreateInfo stagingAllocCreateInfo = {};
+            stagingAllocCreateInfo.usage = VMA_MEMORY_USAGE_AUTO;
+            stagingAllocCreateInfo.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT |
+                                           VMA_ALLOCATION_CREATE_MAPPED_BIT;
+
+            VkBuffer stagingBuf;
+            VmaAllocation stagingAlloc;
+            VmaAllocationInfo stagingAllocInfo;
+            VkResult result =
+                vmaCreateBuffer(m_allocator, &stagingBufCreateInfo, &stagingAllocCreateInfo,
+                                &stagingBuf, &stagingAlloc, &stagingAllocInfo);
+            // Check result...
+
+            // [Executed in runtime]:
+            memcpy(stagingAllocInfo.pMappedData, &uniform, sizeof(T));
+            result = vmaFlushAllocation(m_allocator, stagingAlloc, 0, VK_WHOLE_SIZE);
+            // Check result...
+
+            VkBufferMemoryBarrier bufMemBarrier = {VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER};
+            bufMemBarrier.srcAccessMask = VK_ACCESS_HOST_WRITE_BIT;
+            bufMemBarrier.dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+            bufMemBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+            bufMemBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+            bufMemBarrier.buffer = stagingBuf;
+            bufMemBarrier.offset = 0;
+            bufMemBarrier.size = VK_WHOLE_SIZE;
+
+            vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_HOST_BIT,
+                                 VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, nullptr, 1, &bufMemBarrier,
+                                 0, nullptr);
+
+            VkBufferCopy bufCopy = {
+                0,         // srcOffset
+                0,         // dstOffset,
+                sizeof(T), // size
+            };
+
+            vkCmdCopyBuffer(commandBuffer, stagingBuf, m_buffer, 1, &bufCopy);
+
+            VkBufferMemoryBarrier bufMemBarrier2 = {VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER};
+            bufMemBarrier2.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+            bufMemBarrier2.dstAccessMask =
+                VK_ACCESS_UNIFORM_READ_BIT; // We created a uniform buffer
+            bufMemBarrier2.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+            bufMemBarrier2.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+            bufMemBarrier2.buffer = m_buffer;
+            bufMemBarrier2.offset = 0;
+            bufMemBarrier2.size = VK_WHOLE_SIZE;
+
+            vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT,
+                                 VK_PIPELINE_STAGE_VERTEX_SHADER_BIT, 0, 0, nullptr, 1,
+                                 &bufMemBarrier2, 0, nullptr);
+        }
     }
 
     inline VkBuffer Buffer() const
