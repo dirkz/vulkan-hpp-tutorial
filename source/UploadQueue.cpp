7e07a33536f1dddd4309146122f60939d4e9c1f1
Dirk Zimmermann <me@dirkz.com>
2025-03-29 20:21:10 +0100

    Basic skeleton for upload queue

--- source/UploadQueue.cpp
+++ source/UploadQueue.cpp
@@ -3,4 +3,35 @@
 namespace zvk
 {
 
+UploadQueue::UploadQueue(const vk::Device device, uint32_t uploadQueue) : m_device{device}
+{
+    m_queue = device.getQueue(uploadQueue, 0);
+
+    vk::FenceCreateInfo fenceCreateInfo{};
+    m_fence = device.createFenceUnique(fenceCreateInfo);
+
+    vk::CommandPoolCreateFlags commandPoolCreateFlags =
+        vk::CommandPoolCreateFlagBits::eResetCommandBuffer |
+        vk::CommandPoolCreateFlagBits::eTransient;
+    vk::CommandPoolCreateInfo createInfo{commandPoolCreateFlags, uploadQueue};
+    m_commandPool = device.createCommandPoolUnique(createInfo);
+
+    vk::CommandBufferAllocateInfo allocInfo{m_commandPool.get(), vk::CommandBufferLevel::ePrimary,
+                                            1};
+    auto commandBuffers = device.allocateCommandBuffersUnique(allocInfo);
+    m_commandBuffer = std::move(commandBuffers[0]);
+
+    vk::CommandBufferBeginInfo beginInfo{};
+    m_commandBuffer->begin(beginInfo);
 }
+
+void UploadQueue::FinishAndWait()
+{
+    m_commandBuffer->end();
+    vk::SubmitInfo submitInfo{{}, {}, {*m_commandBuffer}, {}};
+    m_queue.submit({submitInfo}, *m_fence);
+    vk::Result result = m_device.waitForFences({*m_fence}, VK_TRUE, UINT64_MAX);
+    assert(result == vk::Result::eSuccess);
+}
+
+} // namespace zvk
--- source/UploadQueue.h
+++ source/UploadQueue.h
@@ -2,11 +2,24 @@
 
 #include "stdafx.h"
 
+#include "MappedBuffer.h"
+
 namespace zvk
 {
 
 struct UploadQueue
 {
+    UploadQueue(const vk::Device device, uint32_t uploadQueue);
+
+    void FinishAndWait();
+
+  private:
+    vk::Device m_device;
+    vk::UniqueFence m_fence;
+    vk::Queue m_queue;
+    vk::UniqueCommandPool m_commandPool;
+    vk::UniqueCommandBuffer m_commandBuffer;
+    std::vector<std::unique_ptr<MappedBuffer>> m_stagingBuffers;
 };
 
 } // namespace zvk
